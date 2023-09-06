09aa8e66dba752914d1b3d953f864a0c26161d98
root <fury8208@gmail.com>
2023-09-06 19:34:53 +0900

    example

--- py/gandan/gandan/example/test_recv.py
+++ py/gandan/gandan/example/test_recv.py
@@ -1,16 +1,36 @@
 from gandan import MMAP
 import time
 from datetime import datetime as dt
-m = MMAP("/tmp/some.que", 32*1024, 32)
+import logging
+
+logging.basicConfig(
+    filename = f"/tmp/test_recv.log",
+    format ='%(asctime)s:%(levelname)s:%(message)s',
+    datefmt ='%m/%d/%Y %I:%M:%S %p',
+    level = logging.INFO
+)
+        
+m = MMAP("/tmp/some.que", 128*4096, 128)
 gap_total = 0
 gap_count = 0
+
+validity_dict = {}
+
 while True:
     datas = m.readp()
     for data in datas:
         data = str(data, 'ascii')
-        cnt, tv = data.split("|")
+        id, cnt, tv = data.split("|")
         cnt, tv = int(cnt), float(tv)
         gap = (dt.now().timestamp() - tv)
         gap_total += gap; gap_count += 1
-        if gap_count % 10 == 0:
-            print(gap_total/gap_count)
+        # if gap_count % 10 == 0:
+        # logging.error(gap_total/gap_count)
+        #logging.error("%s-%d" % (id, cnt))
+        if not id in validity_dict.keys():
+            validity_dict[id] = cnt
+        else:
+            gap = cnt - validity_dict[id] 
+            if gap != 1:
+                logging.error("error occured in %s, %d, %d - %d" % (id, gap, cnt, validity_dict[id]))
+            validity_dict[id] = cnt
--- py/gandan/gandan/example/test_send.py
+++ py/gandan/gandan/example/test_send.py
@@ -1,10 +1,15 @@
 from gandan import MMAP
-import time
+import time, sys
 from datetime import datetime as dt
-m = MMAP("/tmp/some.que", 32*1024, 32)
+
+m = MMAP("/tmp/some.que", 128*4096, 128)
+
+_id = sys.argv[1]
+_latency = float(sys.argv[2])
+
 cnt = 0
 while True:
     n = dt.now().timestamp()
-    m.writep(bytes(str(cnt)+"|"+str(n), 'ascii'))
+    m.writep(bytes(str(_id) + "|" + str(cnt)+"|"+str(n), 'ascii'))
     cnt += 1
-    time.sleep(1)
+    time.sleep(_latency)
