2071a9e211e95fafb36b8c576e9964ff6a628b52
willcaster0418 <fury8208@gmail.com>
2023-10-05 14:02:56 +0900

    Update setup.py
    minor number change

--- py/gandan/setup.py
+++ py/gandan/setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
 	name="gandan", # Replace with your own username
-	version="0.1.1",
+	version="0.1.2",
 	author="SANG-HUN, KIM",
 	author_email="fury8208@gmail.com",
 	description="a small tcp based middlware",
