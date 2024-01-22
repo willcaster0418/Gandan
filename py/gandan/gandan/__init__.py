6d393dd3e8cdd113335b97cf8b7e360256ea59bd
willcaster0418 <fury8208@gmail.com>
2024-01-22 13:33:34 +0900

    Update __init__.py

--- py/gandan/gandan/__init__.py
+++ py/gandan/gandan/__init__.py
@@ -1,6 +1,12 @@
-from .Gandan import *
-from .GandanMsg import *
-from .GandanPub import *
-from .GandanSub import *
-from .MMAP import *
-print("hello")
+try:
+  from .Gandan import *
+  from .GandanMsg import *
+  from .GandanPub import *
+  from .GandanSub import *
+  from .MMAP import *
+except Exception as e:
+  from gandan.Gandan import *
+  from gandan.GandanMsg import *
+  from gandan.GandanPub import *
+  from gandan.GandanSub import *
+  from gandan.MMAP import *
